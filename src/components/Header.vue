555bacb7fa80d0f22c61c383c3733c3b71d063d5
fvieir <fcvieira2@gmail.com>
2021-12-21 23:01:53 -0300

    Add toolbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>Header</h1>
+ <v-toolbar app>
+   <v-toolbar-title class="headline text-uppercase mr-5">
+     <span>Stock</span>
+     <span class="font-weight-light">Trader</span>
+   </v-toolbar-title>
+   <v-toolbar-items>
+     <v-btn flat to="/">Inicio</v-btn>
+     <v-btn flat to="/portifolio">Portifolio</v-btn>
+     <v-btn flat to="/stocks">Açoes</v-btn>
+   </v-toolbar-items>
+ </v-toolbar>
 </template>
 
 <script>
