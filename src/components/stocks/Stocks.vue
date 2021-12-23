a4725be6ff382699a4f79806260cef756953a937
fvieir <fcvieira2@gmail.com>
2021-12-22 21:32:19 -0300

    Criação componenente Ação

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>Ações</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id"/>
+  </v-layout>
 </template>
 
 <script>
+import Stock from './Stock.vue'
 export default {
-
+  components: {Stock},
+  data () {
+    return {
+      stocks: [
+        {id: 1, name: 'bmw', price: 110},
+        {id: 2, name: 'google', price: 200},
+        {id: 3, name: 'Apple', price: 280},
+        {id: 4, name: 'Twiter', price: 83},
+      ]
+    }
+  }
 }
 </script>
 
