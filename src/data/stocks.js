6d0b79b76034479341d1ecf4a4995f58acdf28e4
fvieir <fcvieira2@gmail.com>
2021-12-26 09:26:12 -0300

    Configurando Vuex e add modules stocks

---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'Bmw', price: 110},
+    {id: 2, name: 'Google', price: 200},
+    {id: 3, name: 'Apple', price: 280},
+    {id: 4, name: 'Twiter', price: 83},
+]
